e5d42d0a7bb32b3a98a5f9e6adc79b8f2843cdf3
sc0Vu <alk03073135@gmail.com>
2018-06-28 11:22:44 +0800

    Add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const validator = require('./lib/validator');
+const confirmed = require('./lib/confirmed');
+const watch = require('./lib/watch');
+
+module.exports = {
+  validator: validator,
+  confirmed: confirmed,
+  watch: watch
+}

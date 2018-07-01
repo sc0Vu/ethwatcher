c4e2fb866aa74df47f6ea1647fca4cca8a82a91f
sc0Vu <alk03073135@gmail.com>
2018-07-01 23:35:02 +0800

    Add ethProvider

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const validator = require('./lib/validator');
 const confirmed = require('./lib/confirmed');
 const watch = require('./lib/watch');
+const Eth = require('web3-eth');
+const watcherLib = {};
 
-module.exports = {
-  validator: validator,
-  confirmed: confirmed,
-  watch: watch
+watcherLib.validator = validator;
+watcherLib.confirmed = confirmed;
+watcherLib.watch = watch;
+watcherLib.ethProvider = function (url) {
+  return new Eth(url);
 }
+
+module.exports = watcherLib;

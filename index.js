a56c51df2486f3cb59aa8a99990762a823f5e1c0
sc0Vu <alk03073135@gmail.com>
2021-02-24 21:50:04 +0800

    Update to ethers

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const validator = require('./lib/validator');
 const confirmed = require('./lib/confirmed');
 const watch = require('./lib/watch');
-const Eth = require('web3-eth');
+const ethers = require('ethers');
 const watcherLib = {};
 
 watcherLib.validator = validator;
 watcherLib.confirmed = confirmed;
 watcherLib.watch = watch;
 watcherLib.ethProvider = function (url) {
-  return new Eth(url);
-}
+  return new ethers.providers.JsonRpcProvider(url);
+i}
 
 module.exports = watcherLib;
